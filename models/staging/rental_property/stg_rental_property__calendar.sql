2416842abcd7d3c997c3f70da50255a5eeb0a665
jaswalwendy@gmail.com <jaswalwendy@gmail.com>
2026-03-07 15:40:13 +0000

    adding staging yml and casted columns

--- models/staging/rental_property/stg_rental_property__calendar.sql
+++ models/staging/rental_property/stg_rental_property__calendar.sql
@@ -1,24 +1,21 @@
-{{
-    config(
-        schema = 'staging_rental_property'
+{{ config(schema="staging_rental_property") }}
+
+with
+    source as (select * from {{ source("rental_property_management", "calendar") }}),
+
+    renamed as (
+        select
+            listing_id,
+            date(date) as calendar_date,
+            reservation_id,
+            case when available = 't' then true else false end AS is_available,
+            cast(
+                replace(replace(price, '$', ''), ',', '') as numeric
+            ) as calendar_price,
+            minimum_nights,
+            maximum_nights
+        from source
     )
-}}
-
-with source as (
-    select * from {{ source("rental_property_management", "calendar") }}
-),
-
-renamed as (
-    select
-        listing_id,
-        reservation_id,
-        date,
-        available as is_available,
-        price,
-        minimum_nights,
-        maximum_nights
-    from source
-)
 
 select *
 from renamed
--- models/staging/rental_property/stg_rental_property__listings.sql
+++ models/staging/rental_property/stg_rental_property__listings.sql
@@ -1,37 +1,32 @@
-{{
-    config(
-        schema = 'staging_rental_property'
+{{ config(schema="staging_rental_property") }}
+
+with
+    source as (select * from {{ source("rental_property_management", "listings") }}),
+
+    renamed as (
+        select
+            id as listing_id,
+            host_id,
+            host_name,
+            date(host_since) as host_since_date,
+            host_location,
+            host_verifications,
+            name as listing_name,
+            neighborhood,
+            property_type,
+            room_type,
+            accommodates,
+            bathrooms_text,
+            cast(bedrooms as integer) as bedrooms,
+            beds,
+            amenities,
+            cast(replace(replace(price, '$', ''), ',', '') as numeric) as listing_price,
+            number_of_reviews,
+            date(first_review) as first_review_date,
+            date(last_review) as last_review_date,
+            cast(review_scores_rating as numeric) as review_scores_rating
+        from source
     )
-}}
-
-with source as (
-    select * from {{ source("rental_property_management", "listings") }}
-),
-
-renamed as (
-    select
-        id as listing_id,
-        host_id,
-        name as listing_name,
-        host_name,
-        date(host_since) AS host_since_date,
-        host_location,
-        host_verifications,
-        neighborhood,
-        property_type,
-        room_type,
-        accommodates,
-        bathrooms_text,
-        bedrooms,
-        beds,
-        amenities,
-        cast(replace(price, '$', '') as numeric) as listing_price,
-        number_of_reviews,
-        DATE(first_review) AS first_review_date,
-        DATE(last_review) AS last_review_date,
-        review_scores_rating
-    from source
-)
 
 select *
 from renamed
